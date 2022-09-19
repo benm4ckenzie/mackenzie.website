ba43a9d3b10a1460eb3bf5eaa10640c7e6e43534
Benjamin Mackenzie <benjaminmackenzie@benjamins-mbp.home>
2022-09-19 15:16:36 +0100

    Refactors code format

--- finance/settings.py
+++ finance/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,6 +62,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.apple",
+    "home",
 ]
 
 SOCIALACCOUNT_PROVIDERS = {
@@ -91,7 +93,10 @@ ROOT_URLCONF = "finance.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
